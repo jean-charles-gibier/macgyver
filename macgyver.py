8fb4a576f0536e3bc3362b71580c6eec3f281fcc
jean-charles-gibier <jeancharlesg@free.fr>
2020-04-06 23:22:01 +0200

    Test 4 pylint checking 1ere correction

--- macgyver.py
+++ macgyver.py
@@ -1,14 +1,22 @@
 #! /usr/bin/env python3
 # coding: utf-8
+
+"""This script starts mc Guyver labyrinth parameter is :  file_name (discribing map)."""
+
 import argparse
 
+# test 4 pylint checks
 def parse_arguments():
+    """Parse_arguments parsing args no parameter."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--datafile",help="""TXT file containing map of
+    parser.add_argument("-d", "--datafile", help="""TXT file containing map of
         labyrinth""")
+    return parser.parse_args()
 
 def main():
+    """Main entry no parameter."""
     args = parse_arguments()
+    return args[0]
 
 if __name__ == "__main__":
     main()
